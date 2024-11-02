fc5530d05e1a94f54ee26d706b87e409c661fb32
svpl-dev <sivapol.l@obec.moe.go.th>
2024-11-02 11:39:04 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBEidj7pxTCrrbgy6TTdeNRlL0HzP8SL3k",
+    authDomain: "educationweb-17ce1.firebaseapp.com",
+    projectId: "educationweb-17ce1",
+    storageBucket: "educationweb-17ce1.firebasestorage.app",
+    messagingSenderId: "807525900610",
+    appId: "1:807525900610:web:dc78c7bb29c9961dc0a8db",
+    measurementId: "G-8XRGQHBPX4"
+  };
